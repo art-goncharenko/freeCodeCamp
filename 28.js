f7e646a17ab8a120ef5297ad04c11ae894f0eb4d
Artem <email@gmail.com>
2022-02-06 01:07:58 -0500

    a

---
+++ 28.js
@@ -0,0 +1,21 @@
+//Write a function nextInLine which takes an array (arr) and a number (item) as arguments.
+
+//Add the number to the end of the array, then remove the first element of the array.
+
+//The nextInLine function should then return the element that was removed.
+
+function nextInLine(arr, item) {
+    // Only change code below this line
+    testArr.push(6);
+    testArr.shift();
+    return item;
+    // Only change code above this line
+  }
+  
+  // Setup
+  const testArr = [1, 2, 3, 4, 5];
+  
+  // Display code
+  console.log("Before: " + JSON.stringify(testArr));
+  console.log(nextInLine(testArr, 6));
+  console.log("After: " + JSON.stringify(testArr));
